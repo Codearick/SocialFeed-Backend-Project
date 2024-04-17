d5fec35d9ff437ab1a07868cf2575f760d307a61
Codearick <rohan.gautam650@gmail.com>
2024-04-18 02:24:36 +0530

    add update of app file

--- src/app.js
+++ src/app.js
@@ -4,9 +4,8 @@ import cors from "cors"
 
 const app = express();
 
-
 app.use(cors({
-    origin: process.origin.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
@@ -14,6 +13,16 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended : true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+//routes import 
+import userRouter from './routes/user.routes.js';
+
+
+//routes declaration 
+app.use("/api/v1/users", userRouter)
+
+//https://localhost:8000/api/v1/users/register
 
 export {app} 
 
