5beb970e95138168cd0c8b6a33ee2703a67202b9
Codearick <rohan.gautam650@gmail.com>
2024-04-22 03:20:06 +0530

    add routes for login logout and refresh-token

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,10 @@
 import {Router} from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { loginUser } from "../controllers/user.controller.js";
+import { logoutUser } from "../controllers/user.controller.js";
+import { refreshAccessToken } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -17,5 +21,14 @@ router.route("/register").post(
     ]),
     registerUser
 )
+
+
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/refresh-token"),post(refreshAccessToken)
+
+
 
 export default router;
