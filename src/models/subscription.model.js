1c21271b4e889a875db56a8a12d0ccef3e9f997f
Codearick <rohan.gautam650@gmail.com>
2024-04-26 03:31:22 +0530

    add comment, like, playlist and tweet models

---
+++ src/models/subscription.model.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose"
+
+const subscriptionSchema = new Schema(
+    {
+        subscriber: {
+            type: Schema.Types.ObjectId, // One who is subscribing
+            ref: "Users",
+        },
+        channel: {
+            type: Schema.Types.ObjectId, 
+            ref: "Users"
+        },
+    },
+    {
+        timestamps : true
+    }
+)
+
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema)
