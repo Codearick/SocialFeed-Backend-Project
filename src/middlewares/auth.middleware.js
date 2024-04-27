bba5a5192f77eaf7c270f4c2bbdf5644113cb89d
Codearick <rohan.gautam650@gmail.com>
2024-04-28 02:40:36 +0530

    add auth middleware bugfixes

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -14,7 +14,7 @@ export const verifyJWT = asyncHandler( async(req, _, next) => {
  
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
  
-    const user = User.findById(decodedToken?._id).select("-password -refreshToken")
+    const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
  
     if(!user){
      //TODO: Discuss about frontend
