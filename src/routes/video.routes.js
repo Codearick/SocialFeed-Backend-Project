632b287c55e44809c607e4c9c2ca4f05b8bedf06
Codearick <rohan.gautam650@gmail.com>
2024-05-04 14:13:42 +0530

    add update on video routes

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -7,25 +7,25 @@ import {
     togglePublishStatus,
     updateVideo,
 } from "../controllers/video.controller.js"
-import {verifyJWT} from "../middlewares/auth.middleware.js"
-import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js"
+import { upload } from "../middlewares/multer.middleware.js"
 
 const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route("/").get(getAllVideos).post( upload.fields([
-            {
-                name: "videoFile",
-                maxCount: 1,
-            },
-            {
-                name: "thumbnail",
-                maxCount: 1,
-            },
-            
-        ]),
-        publishAVideo
-    );
+router.route("/").get(getAllVideos).post(upload.fields([
+    {
+        name: "videoFile",
+        maxCount: 1,
+    },
+    {
+        name: "thumbnail",
+        maxCount: 1,
+    },
+
+]),
+    publishAVideo
+);
 
 router
     .route("/:videoId")
