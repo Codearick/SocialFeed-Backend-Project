eba42d70c732053b9b1328a7287ebff8f814c1a7
Codearick <rohan.gautam650@gmail.com>
2024-05-06 03:24:15 +0530

    add comment routes

---
+++ src/routes/comment.routes.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import {
+    addComment,
+    deleteComment,
+    getVideoComments,
+    updateComment,
+} from "../controllers/comment.controller.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
+
+const router = Router();
+
+router.use(verifyJWT);
+
+router.route("/:videoId").get(getVideoComments).post(addComment);
+router.route("/c/:commentId").patch(updateComment).delete(deleteComment);
+
+export default router;
+
