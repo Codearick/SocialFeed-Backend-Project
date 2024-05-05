05d7729a5dd4793721e726fa6be3f112e7efd87c
Codearick <rohan.gautam650@gmail.com>
2024-05-06 03:22:55 +0530

    add comment routes in the app

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ import userRouter from './routes/user.routes.js';
 import tweetRouter from "./routes/tweet.routes.js";
 import healthRouter  from "./routes/healthcheck.routes.js";
 import videoRouter from "./routes/video.routes.js";
+import commentRouter from "./routes/comment.routes.js"
 
 
 //routes declaration 
@@ -27,6 +28,7 @@ app.use("/api/v1/users", userRouter)
 app.use("/api/v1/tweets", tweetRouter)
 app.use("api/v1/healthcheck", healthRouter)
 app.use("api/v1/videos", videoRouter)
+app.use("api/v1/comments", commentRouter)
 
 //https://localhost:8000/api/v1/users/register
 
