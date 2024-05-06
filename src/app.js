827828cb7f38d72e8a5c05e792d6063fe9acadef
Codearick <rohan.gautam650@gmail.com>
2024-05-07 03:50:38 +0530

    add routes for likes

--- src/app.js
+++ src/app.js
@@ -20,7 +20,8 @@ import userRouter from './routes/user.routes.js';
 import tweetRouter from "./routes/tweet.routes.js";
 import healthRouter  from "./routes/healthcheck.routes.js";
 import videoRouter from "./routes/video.routes.js";
-import commentRouter from "./routes/comment.routes.js"
+import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
 
 
 //routes declaration 
@@ -29,8 +30,8 @@ app.use("/api/v1/tweets", tweetRouter)
 app.use("api/v1/healthcheck", healthRouter)
 app.use("api/v1/videos", videoRouter)
 app.use("api/v1/comments", commentRouter)
+app.use("api/v1/likes", likeRouter)
 
 //https://localhost:8000/api/v1/users/register
 
-export {app} 
-
+export {app}
