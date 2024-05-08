6c8175711bcada8eb8f53c8229c919d06c5037c1
Codearick <rohan.gautam650@gmail.com>
2024-05-09 03:52:16 +0530

    add subscription routes and dashboard routes in the app file

--- src/app.js
+++ src/app.js
@@ -22,6 +22,8 @@ import healthRouter  from "./routes/healthcheck.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import likeRouter from "./routes/like.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 
 
 //routes declaration 
@@ -31,6 +33,8 @@ app.use("api/v1/healthcheck", healthRouter)
 app.use("api/v1/videos", videoRouter)
 app.use("api/v1/comments", commentRouter)
 app.use("api/v1/likes", likeRouter)
+app.use("api/v1/subscriptions", subscriptionRouter)
+app.use("api/v1/dashboard", dashboardRouter)
 
 //https://localhost:8000/api/v1/users/register
 
