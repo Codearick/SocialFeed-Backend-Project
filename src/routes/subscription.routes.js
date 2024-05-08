85eb5ac90f5450d17efbbce4513fb046ab0f55e7
Codearick <rohan.gautam650@gmail.com>
2024-05-09 03:51:47 +0530

    add subscripton routes code

---
+++ src/routes/subscription.routes.js
@@ -0,0 +1,16 @@
+import Router from "express"
+import {
+    toggleSubscription,
+    getUserChannelSubscribers,
+    getSubscribedChannels
+} from "../controllers/subscription.controller.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js"
+
+const router = Router();
+router.use(verifyJWT);
+
+router.route("/c/:channelId").post(toggleSubscription).get(getSubscribedChannels)
+
+router.route("/u/:subscriberId").get(getUserChannelSubscribers)
+
+export default router
