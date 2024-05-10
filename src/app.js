bdb51c03b8c8ed8c961f32ce2cf46d4410b9f0aa
Codearick <rohan.gautam650@gmail.com>
2024-05-11 04:19:18 +0530

    add bug fixes in the routes declaration in app.js file

--- src/app.js
+++ src/app.js
@@ -24,18 +24,21 @@ import commentRouter from "./routes/comment.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js"
 import dashboardRouter from "./routes/dashboard.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
 
 
 //routes declaration 
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/tweets", tweetRouter)
-app.use("api/v1/healthcheck", healthRouter)
-app.use("api/v1/videos", videoRouter)
-app.use("api/v1/comments", commentRouter)
-app.use("api/v1/likes", likeRouter)
-app.use("api/v1/subscriptions", subscriptionRouter)
-app.use("api/v1/dashboard", dashboardRouter)
+app.use("/api/v1/healthcheck", healthRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
+app.use("/api/v1/playlist", playlistRouter)
 
 //https://localhost:8000/api/v1/users/register
 
 export {app}
+
