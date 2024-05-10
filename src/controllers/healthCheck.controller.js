f555989229e7097501a0a864e5469c39d11f04c4
Codearick <rohan.gautam650@gmail.com>
2024-05-11 04:19:50 +0530

    add healthcheck changes and bug fixes

--- src/controllers/healthCheck.controller.js
+++ src/controllers/healthCheck.controller.js
@@ -3,7 +3,7 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const healthCheck = asyncHandler( async(_, res) => {
     //TODO: build a healthcheck response that simply returns the OK status as json with a message
-    const response = new ApiResponse(200, {}, "This is a health check and your health is Okaishh!");
+    const response = new ApiResponse(200, "This is a health check and your health is Okaishh!");
     return res.status(200).json(response);
 })
 
