af50b0c01c2f16845439b0f8f1cf1504fca51636
Codearick <rohan.gautam650@gmail.com>
2024-05-12 05:15:04 +0530

    add playlist routes bug fixes

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,4 +1,4 @@
-import Router from "express"
+import { Router }  from "express"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 import {
     addVideoToPlaylist,
@@ -25,3 +25,5 @@ router.route("/add/:videoId/:playlistId").patch(addVideoToPlaylist)
 router.route("/remove/:videoId/:playlistId").patch(removeVideoFromPlaylist);
 
 router.route("/user/:userId").get(getUserPlaylists);
+
+export default router;
