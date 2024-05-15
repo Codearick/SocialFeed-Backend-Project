c5b05cece3b8766e9450cd6516c69f18f70be8f1
Codearick <rohan.gautam650@gmail.com>
2024-05-16 04:33:15 +0530

    add index in tweet model for text based search

--- src/models/tweet.model.js
+++ src/models/tweet.model.js
@@ -1,4 +1,5 @@
 import mongoose, {Schema} from "mongoose"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
 
 const tweetSchema = new Schema(
     {
@@ -11,7 +12,13 @@ const tweetSchema = new Schema(
             ref: "User"
         }
     },
-    {timestamps: true}
+    { timestamps: true }
 )
 
+tweetSchema.index(
+    { content: "text", owner: "text" },
+    { name: "search_tweeter", weights: { content: 3, owner: 1 } }
+);
+
+tweetSchema.plugin(mongooseAggregatePaginate)
 export const Tweet = mongoose.model("Tweet", tweetSchema)
